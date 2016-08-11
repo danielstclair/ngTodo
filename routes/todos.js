216171d4aa92a55def052213b35681b1eca9ac69
DANIEL A. ST CLAIR <danielstclair@gmail.com>
2016-08-11 14:41:05 -0500

    Finished CRUD server
    finished all CRUD routes to mongo db server

---
+++ routes/todos.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://admin:admin@ds153815.mlab.com:53815/ngtodoapp', ['todos']);
+
+// Get all todos
+router.get('/todos', function(req, res, next) {
+	db.todos.find(function(err, todos) {
+		if(err) {
+			res.send(err);
+		}
+		else {
+			res.json(todos);
+		}
+	})
+});
+
+// Get single todo
+router.get('/todo/:id', function(req, res, next) {
+	db.todos.findOne({
+		_id: mongojs.ObjectId(req.params.id)
+	}, function(err, todo) {
+		if (err) {
+			res.send(err);
+		}
+		else {
+			res.json(todo);
+		}
+	})
+})
+
+// Save Todo
+router.post('/todo', function(req, res, next) {
+	var todo = req.body;
+	if (!todo.text || !(todo.isComponent + '')) {
+		res.status(400);
+		res.json({
+			"error": "Invalid Data"
+		});
+	}
+	else {
+		db.todos.save(todo, function(err, result) {
+			if (err) {
+				res.send(err);
+			}
+			else {
+				res.json(result);
+			}
+		});
+	}
+});
+
+// Update Todo
+
+router.put('/todo/:id', function(req, res, next) {
+	var todo = req.body;
+	var updObj = {};
+	if (todo.isCompleted) {
+		updObj.isCompleted = todo.isCompleted;
+	}
+	if (todo.text) {
+		updObj.text = todo.text;
+	}
+	if (!updObj) {
+		res.status(400);
+		res.json({
+			"error": "Invalid Data"
+		});
+	}
+	else {
+		db.todos.update({
+			_id: mongojs.ObjectId(req.params.id)
+		}, updObj, {}, function(err, result) {
+			if (err) {
+				res.send(err);
+			}
+			else {
+				res.json(result);
+			}
+		});
+	}
+});
+
+// Delete todos
+
+router.delete('/todo/:id', function(req, res, next) {
+	db.todos.remove({
+		_id: mongojs.ObjectId(req.params.id)
+	}, '', function(err, result) {
+		if (err) {
+			res.send(err);
+		}
+		else {
+			res.json(result);
+		}
+	})
+})
+
+module.exports = router;
