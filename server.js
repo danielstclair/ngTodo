24c929abba29673c349e16eb1a2bf360a41ac3a9
DANIEL A. ST CLAIR <danielstclair@gmail.com>
2016-09-01 16:30:06 -0500

    Finished all routes for adding, editing, and deleting todos. Created client side folder with necessary Angular 2 setup. Started static template for application.

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, 'client')));
 
 app.use(bodyParser.json());
